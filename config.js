822f3b59dd6654a243a09d496dc2f250e1bb8f63
xidizxc <66018531+xidizxc@users.noreply.github.com>
2022-04-07 19:41:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   fromDisplayText: '哥哥', // 收件箱展示的来件人名字
   fromDisplaySubText: '每日爱你提醒', // 收件箱展示的次级标题
   user: 'zxc28432@163.com', // 发送者邮箱
-  pass: 'CCALSVQDEXDHHLFC', // 发送者邮箱MTP协议密码
+  pass: '${{ secrets.MAILUSERNAME }}', // 发送者邮箱MTP协议密码
   to: '2843250141@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101220101',  // 和风天气-合肥的id
