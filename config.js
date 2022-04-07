204bf8437f98c712a737bbaceef4abf24952e1fa
xidizxc <66018531+xidizxc@users.noreply.github.com>
2022-04-07 19:32:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // 配置信息
 module.exports = {
-  fromDisplayText: '阳哥', // 收件箱展示的来件人名字
-  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
-  user: 'xxx', // 发送者邮箱
-  pass: 'xxx', // 发送者邮箱MTP协议密码
-  to: 'xxx', // 发送到谁，填邮箱
+  fromDisplayText: '哥哥', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日爱你提醒', // 收件箱展示的次级标题
+  user: 'zxc28432@163.com', // 发送者邮箱
+  pass: 'CCALSVQDEXDHHLFC', // 发送者邮箱MTP协议密码
+  to: '2843250141@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: 'xxx',  // 和风天气-成都武侯区的id
+  location: '101220101',  // 和风天气-合肥的id
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
-  startDay: '2015-09-29', // 在一起的日期
+  startDay: '2020-08-22', // 在一起的日期
 }
